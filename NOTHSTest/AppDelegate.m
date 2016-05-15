ab34235b83e56cc8f5c6d228cab7d5c1eabbbe87
Belén Molina del Campo <bmolinadelcampo@gmail.com>
2016-05-15 19:26:41 +0100

    Revert changes to AppDelegate.

--- NOTHSTest/AppDelegate.m
+++ NOTHSTest/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    return NO;
+    return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
